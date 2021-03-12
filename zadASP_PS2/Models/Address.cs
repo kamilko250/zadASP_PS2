52f45af21bcf1e61bd46713062fd9acdc2cfae07
kamilko250 <freshmultikk21@gmail.com>
2021-03-12 11:54:13 +0100

    Add project files.

---
+++ zadASP_PS2/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadASP_PS2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string Street { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string  ZipCode { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int Number { get; set; }
+    }
+}
---
+++ zadASP_PS2/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using zadASP_PS2.Models;
+
+namespace zadASP_PS2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string  Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        
+        }
+    }
+}
